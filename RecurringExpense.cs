e708a27a2af962d1caec687d59225fe4e28ad8c0
AlexZmasser <63077643+AlexZmasser@users.noreply.github.com>
2024-02-08 20:30:58 +1100

    update files

--- RecurringExpense.cs
+++ RecurringExpense.cs
@@ -6,7 +6,9 @@ namespace PAYSCHEDULER
     public class RecurringExpense : Expenses
     {
         public RecurringExpense( decimal TotalCost, DateTime StartDate, String Group, bool Paid): base(TotalCost,StartDate,Group,Paid)
-        {}
+        {
+            
+        }
     }
 
 
